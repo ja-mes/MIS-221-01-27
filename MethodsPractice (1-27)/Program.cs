c6909ca6b56f91cb5d6602a1aafd312fc897516e
unknown <jamezbrown100@gmail.com>
2020-01-27 17:26:54 -0600

    add methods

--- MethodsPractice (1-27)/Program.cs
+++ MethodsPractice (1-27)/Program.cs
@@ -6,7 +6,23 @@ namespace MethodsPractice__1_27_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string presidentName = GetPresidentName();
+            WelcomeMessage(presidentName); // method call
+
+            Console.ReadKey();
+        }
+
+        static string GetPresidentName()
+        {
+            Console.Write("Please enter the name of the president: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        // make sure you define methods inside the class
+        static void WelcomeMessage(string name) // method definition
+        {
+            Console.WriteLine("Welcome to AIMS! My name is " + name);
         }
     }
 }
