212ced1607ed7c3b21bc056de6975f96d4ed6b29
unknown <jamezbrown100@gmail.com>
2020-01-27 17:37:38 -0600

    Add company methods

--- MethodsPractice (1-27)/Program.cs
+++ MethodsPractice (1-27)/Program.cs
@@ -7,10 +7,20 @@ namespace MethodsPractice__1_27_
         static void Main(string[] args)
         {
             string presidentName = GetPresidentName();
+            string companyName = GetCompanyName();
             WelcomeMessage(presidentName); // method call
+            WelcomeMessage(companyName);
 
             Console.ReadKey();
         }
+
+        static string GetCompanyName()
+        {
+            Console.Write("Please enter the name of the company: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
 
         static string GetPresidentName()
         {
