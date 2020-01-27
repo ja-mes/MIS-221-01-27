55b1555b45c6f1e38d6d009ec08bd8f4bd5a28a7
unknown <jamezbrown100@gmail.com>
2020-01-27 17:15:26 -0600

    inital

---
+++ MethodsPractice (1-27)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodsPractice__1_27_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
